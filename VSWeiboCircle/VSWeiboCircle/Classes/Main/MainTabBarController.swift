99b13fe8190a66571a96729198b1089409c03c79
VesperHan <447636737@qq.com>
2016-12-05 15:43:08 +0800

    - 按钮触发完成 关闭tabbar按钮 使用自定义button

--- VSWeiboCircle/VSWeiboCircle/Classes/Main/MainTabBarController.swift
+++ VSWeiboCircle/VSWeiboCircle/Classes/Main/MainTabBarController.swift
@@ -41,32 +41,37 @@ extension MainTabBarController {
         
         tabBar.addSubview(composeBtn)
         composeBtn.center = CGPoint(x:tabBar.center.x,y:tabBar.bounds.size.height*0.5)
+        
+        //监听事件点击
         composeBtn.addTarget(self, action: #selector(composeClick(sender:)), for: .touchUpInside)
     }
     
-    @objc private func composeClick(sender:UIButton){
-    
-        self.selectedIndex = 2
-    }
-    
     fileprivate func setUpTabbarBtn(){
-    
         //使用storybaor时时候 卸载didload会重新调整过来,所以要鞋子啊appear里面
         //手动设置选中tabbar的图片
         //因为选中图片也可以storyboard里面进行设置,所以无需用代码重写
         for i in 0..<tabBar.items!.count {
-            
             //获取item
             let item = tabBar.items![i]
             
             //如果下标为2 item不可交互
             if i == 2 {
-                
-                item.isEnabled = true
+                item.isEnabled = false
                 return
             }
             //设置item其他选中图片
             //item.selectedImage = UIImage.init(named: imageNames[i])
         }
+    }
+}
+
+// MARK:- 事件监听
+// 事件监听的本质是发送消息,但是发送消息是OC的特性
+// 方法包装成@SEL-->类中查找方法列表--> 根据@SEL找到imp指针(函数指针)-->执行函数->如果方法是private,函数就不会在方法列表中
+extension MainTabBarController {
+
+    @objc fileprivate func composeClick(sender:UIButton){
+        
+        self.selectedIndex = 2
     }
 }
